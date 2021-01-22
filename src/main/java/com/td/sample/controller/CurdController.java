c72e18ec13bc5e014a4b50e8d971563bc52810af
Tukaram Dhage <trdhage@outlook.com>
2021-01-23 00:07:13 +0530

    API Documentation and findByName using Repository

--- src/main/java/com/td/sample/controller/CurdController.java
+++ src/main/java/com/td/sample/controller/CurdController.java
@@ -2,8 +2,10 @@ package com.td.sample.controller;
 
 import com.td.sample.model.Department;
 import com.td.sample.repository.DepartmentRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -12,18 +14,33 @@ public class CurdController {
     @Autowired
     private DepartmentRepository departmentRepository;
 
+    @GetMapping("/")
+    @ApiOperation(value = "Redirect to Swagger", hidden = true)
+    public RedirectView home() {
+        return new RedirectView("swagger-ui/index.html");
+    }
+
+    @GetMapping("/getDept/{deptName}")
+    @ApiOperation(value = "Get Departments By Name")
+    public List<Department> findDeptByName(@PathVariable String deptName) {
+        return departmentRepository.findDepartmentByName(deptName);
+    }
+
     @GetMapping("/getAllDept")
+    @ApiOperation(value = "Get All Departments")
     public List<Department> getAllDept() {
         return departmentRepository.findAll();
     }
 
     @PostMapping("/createDept")
+    @ApiOperation(value = "Create New Departments")
     public Department createDept(@RequestBody Department department) {
         departmentRepository.save(department);
         return department;
     }
 
     @PutMapping("/updateDept/{deptId}")
+    @ApiOperation(value = "Update Departments")
     public Department updateDept(@RequestBody Department department, @PathVariable String deptId) {
         department.setId(deptId);
         departmentRepository.save(department);
@@ -31,6 +48,7 @@ public class CurdController {
     }
 
     @DeleteMapping("/deleteDept/{deptId}")
+    @ApiOperation(value = "Delete Departments")
     public String deleteDept(@PathVariable String deptId) {
         departmentRepository.deleteById(deptId);
         return deptId;
--- src/main/java/com/td/sample/repository/DepartmentRepository.java
+++ src/main/java/com/td/sample/repository/DepartmentRepository.java
@@ -4,7 +4,10 @@ import com.td.sample.model.Department;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepartmentRepository extends MongoRepository<Department, String> {
 
+    List<Department> findDepartmentByName(String deptName);
 }
